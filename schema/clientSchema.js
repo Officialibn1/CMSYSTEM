1ad667b9dfd17a6daeed0291f7afaab72e403d02
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-07-01 14:46:47 +0100

    Created Client mutation

--- schema/clientSchema.js
+++ schema/clientSchema.js
@@ -1,5 +1,4 @@
-const { clients } = require('../sampledb.js')
-
+const Client = require('../config/models/Client')
 const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLList, GraphQLNonNull } = require('graphql')
 
 const ClientType = new GraphQLObjectType({
@@ -17,17 +16,59 @@ const ClientQuery = {
         type: ClientType,
         args: { id: { type: new GraphQLNonNull(GraphQLID) } },
         resolve(parent, args) {
-            return clients.find(client => client.id === args.id)
+            return Client.findBtId(args.id)
         }
     },
     clients: {
         type: new GraphQLList(ClientType),
         resolve(parent, args) {
-            return clients
+            return Client.find()
+        }
+    }
+}
+
+const ClientMutation = {
+    addClient: {
+        type: ClientType,
+        args: {
+            name: { type: new GraphQLNonNull(GraphQLString) },
+            email: { type: new GraphQLNonNull(GraphQLString) },
+            phone: { type: new GraphQLNonNull(GraphQLString) },
+        },
+        resolve(parent, args) {
+            const client = new Client({
+                name: args.name,
+                email: args.email,
+                phone: args.phone,
+            })
+
+            return client.save().catch(error => {
+                console.error(`Error Creating Client: ${error}`)
+                throw error
+            })
+        }
+    },
+    deleteClient: {
+        type: ClientType,
+        args: {
+            id: { type: new GraphQLNonNull(GraphQLID) }
+        },
+        resolve(parent, args) {
+            return Client.findByIdAndDelete(args.id)
+                .then(deleteClient => {
+                    if (!deleteClient) {
+                        throw new Error(`Client with ID: ${args.id} not found`)
+                    }
+
+                    return deleteClient
+                })
+                .catch(error => {
+                    console.error(`Error Deleting Client: ${error}`)
+                    throw error
+                })
         }
     }
 
-
 }
 
-module.exports = { ClientType, ClientQuery }
+module.exports = { ClientType, ClientQuery, ClientMutation }
