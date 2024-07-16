2788daf04d6c179a705ee70ad90b6a5a69feccba
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-07-16 23:37:48 +0100

    Adjusted Project GraphQL schema to acomodate the new features and security checks

--- schema/projectsSchema.js
+++ schema/projectsSchema.js
@@ -2,6 +2,7 @@
 
 const Client = require('../config/models/Client.js')
 const Project = require('../config/models/Project.js')
+const User = require('../config/models/User.js')
 
 const {
     GraphQLObjectType,
@@ -9,7 +10,8 @@ const {
     GraphQLString,
     GraphQLEnumType,
     GraphQLList,
-    GraphQLNonNull
+    GraphQLNonNull,
+    GraphQLInt
 } = require('graphql')
 const { AuthenticationError } = require('../config/errors/authenticationError.js')
 
@@ -18,18 +20,34 @@ const ProjectType = new GraphQLObjectType({
     name: 'Project',
     fields: () => {
         const { ClientType } = require('./clientSchema.js')
+        const { UserType } = require('./userSchema.js')
 
         return {
             id: { type: GraphQLID },
             name: { type: GraphQLString },
             description: { type: GraphQLString },
             status: { type: GraphQLString },
+            budget: { type: GraphQLInt },
             client: {
                 type: ClientType,
-                async resolve(parent, args) {
+                async resolve(parent, args, context) {
+                    if (!context.user) {
+                        throw new AuthenticationError();
+                    }
+
                     return await Client.findById(parent.clientId)
                 }
-            }
+            },
+            user: {
+                type: UserType,
+                async resolve(parent, args, context) {
+                    if (!context.user) {
+                        throw new AuthenticationError();
+                    }
+
+                    return await User.findById(parent.clientId)
+                }
+            },
         }
     }
 })
@@ -44,7 +62,9 @@ const ProjectQuery = {
                 throw new AuthenticationError()
             }
 
-            return await Project.findById(args.id)
+            const userUID = await context.user?.uid
+
+            return await Project.findOne({ _id: args.id, userUID })
         }
     },
     projects: {
@@ -55,7 +75,10 @@ const ProjectQuery = {
                 throw new AuthenticationError()
             }
 
-            return await Project.find()
+            const userUID = await context.user?.uid
+
+
+            return await Project.find({ userUID })
         }
     }
 }
@@ -66,6 +89,7 @@ const ProjectMutation = {
         args: {
             name: { type: new GraphQLNonNull(GraphQLString) },
             description: { type: new GraphQLNonNull(GraphQLString) },
+            budget: { type: new GraphQLNonNull(GraphQLInt) },
             status: {
                 type: new GraphQLEnumType({
                     name: 'ProjectStatus',
@@ -82,21 +106,30 @@ const ProjectMutation = {
         },
         async resolve(parent, args, context) {
 
-            if (!context.user) {
-                throw new AuthenticationError()
+            try {
+                if (!context.user) {
+                    throw new AuthenticationError()
+                }
+
+                const userUID = await context?.user.uid
+
+
+                const project = new Project({
+                    name: args.name,
+                    description: args.description,
+                    status: args.status,
+                    clientId: args.clientId,
+                    budget: args.budget,
+                    userUID
+                })
+
+                return await project.save()
+
+            } catch (error) {
+                console.error(`Error creating project: ${JSON.stringify(error, null, 2)}`);
+
+                throw new Error(error)
             }
-
-            const project = new Project({
-                name: args.name,
-                description: args.description,
-                status: args.status,
-                clientId: args.clientId,
-            })
-
-            return project.save().catch(error => {
-                console.error(`Error creating project: ${error}`);
-                throw error
-            })
         }
     },
     deleteProject: {
@@ -109,18 +142,22 @@ const ProjectMutation = {
                 throw new AuthenticationError()
             }
 
-            return Project.findByIdAndDelete(args.id)
-                .then(deleteProject => {
-                    if (!deleteProject) {
-                        throw new Error(`Project with ID: ${args.id} not found`)
-                    }
+            try {
+                const userUID = await context?.user.uid
 
-                    return deleteProject
-                })
-                .catch(error => {
-                    console.error(`Error Deleting Project: ${error}`)
-                    throw error
-                })
+                const deleteProject = await Project.findOneAndDelete({ _id: args.id, userUID })
+
+                if (!deleteProject) {
+                    throw new Error(`Project with ID: ${args.id} not found`)
+                }
+
+                return deleteProject
+
+            } catch (error) {
+                console.error(`Error Deleting Project: ${JSON.stringify(error, null, 2)}`)
+
+                throw new Error(error)
+            }
         }
     },
     updateProject: {
@@ -128,6 +165,7 @@ const ProjectMutation = {
         args: {
             id: { type: new GraphQLNonNull(GraphQLID) },
             name: { type: GraphQLString },
+            budget: { type: new GraphQLNonNull(GraphQLInt) },
             description: { type: GraphQLString },
             status: {
                 type: new GraphQLEnumType({
@@ -146,37 +184,44 @@ const ProjectMutation = {
                 throw new AuthenticationError()
             }
 
-            return Project.findById(args.id)
-                .then(previousProject => {
-                    if (!previousProject) {
-                        throw new Error(`Project with ID: ${args.id} not found`)
-                    }
+            try {
+                const userUID = await context?.user.uid
 
-                    return Project.findByIdAndUpdate(
-                        args.id,
-                        {
-                            $set: {
-                                name: args.name || previousProject.name,
-                                description: args.description || previousProject.description,
-                                status: args.status || previousProject.status,
-                                clientId: args.clientId || previousProject.clientId
-                            },
+                const previousProject = await Project.findOne({ _id: args.id, userUID })
 
+                if (!previousProject) {
+                    throw new Error(`Project with ID: ${args.id} not found`)
+                }
+
+                const updateProject = await Project.findOneAndUpdate({
+                    _id: args.id,
+                    userUID
+                },
+                    {
+                        $set: {
+                            name: args.name || previousProject.name,
+                            description: args.description || previousProject.description,
+                            status: args.status || previousProject.status,
+                            clientId: args.clientId || previousProject.clientId,
+                            budget: args.budget || previousProject.budget,
+                            userUID
                         },
-                        { new: true, runValidators: true }
-                    )
-                })
-                .then(updateProject => {
-                    if (!updateProject) {
-                        throw new Error(`Failed to update Project with ID: ${args.id}`)
-                    }
 
-                    return updateProject
-                }).catch(error => {
-                    console.error(`Error updating Project: ${error}`);
+                    },
+                    { new: true, runValidators: true }
+                )
 
-                    throw error
-                })
+                if (!updateProject) {
+                    throw new Error(`Failed to update Project with ID: ${args.id}`)
+                }
+
+                return updateProject
+
+            } catch (error) {
+                console.error(`Error updating Project: ${JSON.stringify(error, null, 2)}`);
+
+                throw new Error(error)
+            }
         }
     }
 }
