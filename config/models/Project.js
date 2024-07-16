9bb08462ff540aa35c355595e961454ab09e8f84
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-07-16 23:37:29 +0100

    Adjust Project mongoose schema

--- config/models/Project.js
+++ config/models/Project.js
@@ -2,22 +2,31 @@ const mongoose = require('mongoose')
 
 const ProjectSchema = new mongoose.Schema({
     name: {
-        type: String
+        type: String,
+        required: true,
     },
     description: {
-        type: String
+        type: String,
+        required: true,
     },
     status: {
         type: String,
-        enum: ['Not Started', 'In Progress', 'Completed']
+        enum: ['Not Started', 'In Progress', 'Completed'],
+        required: true,
+    },
+    budget: {
+        type: Number,
+        required: true,
     },
     clientId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Client'
+        ref: 'Client',
+        required: true,
     },
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+    userUID: {
+        type: String,
+        ref: 'User',
+        required: true
     }
 })
 
