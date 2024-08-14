eae252b2b47a83b687f98450bb22bfe3cb159596
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-08-14 18:22:20 +0100

    chore: Cleaned some redundant files in the index.js file

--- index.js
+++ index.js
@@ -1,116 +1,85 @@
 // https://cloud.hasura.io/public/graphiql?endpoint=http%3A%2F%2Flocalhost%3A5000%2Fgraphql
 
+const express = require("express");
 
-const express = require('express')
+const colors = require("colors");
 
-const colors = require('colors')
+const { ApolloServer } = require("@apollo/server");
 
-// const { createHandler } = require("graphql-http/lib/use/express")
+const { expressMiddleware } = require("@apollo/server/express4");
 
-const { ApolloServer } = require('@apollo/server')
+const schema = require("./schema/rootSchema");
 
-const { expressMiddleware } = require('@apollo/server/express4')
+const connectDB = require("./config/db");
 
-const schema = require('./schema/rootSchema')
+const cors = require("cors");
 
-const connectDB = require('./config/db')
+require("dotenv").config();
 
-const cors = require('cors')
+const serviceAccountKey = require("./config/serviceAccountKey.json");
 
-require('dotenv').config()
+const admin = require("firebase-admin");
 
-const serviceAccountKey = require('./config/serviceAccountKey.json')
+const authenticate = require("./config/middleware/authenticate.js");
 
-const admin = require('firebase-admin');
-
-const authenticate = require('./config/middleware/authenticate.js')
-
-const { AuthenticationError } = require('./config/errors/authenticationError.js')
+const {
+  AuthenticationError,
+} = require("./config/errors/authenticationError.js");
 
 colors.enable();
 
+const port = process.env.PORT || 5000;
+const app = express();
 
-const port = process.env.PORT || 5000
-const app = express()
-
-
-connectDB()
+connectDB();
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccountKey)
-})
+  credential: admin.credential.cert(serviceAccountKey),
+});
 
-app.use(cors())
-app.use(express.json())
-app.use(authenticate)
+app.use(cors());
+app.use(express.json());
+app.use(authenticate);
 
 const server = new ApolloServer({
-    schema,
-    formatError: (err) => {
-        if (err.originalError instanceof AuthenticationError) {
-            return {
-                message: err.message,
-                extensions: {
-                    code: err.originalError.code,
-                    status: err.originalError.status
-                }
-            }
-        }
-
-        return {
-            message: err.message,
-            extensions: {
-                code: 'INTERNAL_SERVER_ERROR',
-                status: 500
-            }
-        }
+  schema,
+  formatError: (err) => {
+    if (err.originalError instanceof AuthenticationError) {
+      return {
+        message: err.message,
+        extensions: {
+          code: err.originalError.code,
+          status: err.originalError.status,
+        },
+      };
     }
-})
+
+    return {
+      message: err.message,
+      extensions: {
+        code: "INTERNAL_SERVER_ERROR",
+        status: 500,
+      },
+    };
+  },
+});
 
 async function startServer() {
-    await server.start()
+  await server.start();
 
-    app.use(
-        '/graphql',
-        expressMiddleware(server, {
-            context: async ({ req }) => {
-                return { user: req.user }
-            }
-        })
-    )
+  app.use(
+    "/graphql",
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        return { user: req.user };
+      },
+    }),
+  );
 
-    app.listen(port, console.log(`SERVER RUNNING ON PORT: http://localhost:${port}`))
+  app.listen(
+    port,
+    console.log(`SERVER RUNNING ON PORT: http://localhost:${port}`),
+  );
 }
 
-startServer()
-
-
-// app.use(
-//     '/graphql',
-//     createHandler({
-//         schema,
-//         context: (req) => ({ user: req.user }),
-//         formatError: (err) => {
-//             if (err.originalError instanceof AuthenticationError) {
-//                 return {
-//                     message: err.message,
-//                     extensions: {
-//                         code: err.originalError.code,
-//                         status: err.originalError.status
-//                     }
-//                 }
-//             }
-
-//             return {
-//                 message: err.message,
-//                 extensions: {
-//                     code: 'INTERNAL_SERVER_ERROR',
-//                     status: 500
-//                 }
-//             }
-//         }
-//     })
-// )
-
-
-
+startServer();
