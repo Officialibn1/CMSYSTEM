e9c302864305ee91ab950f8143a6727c552518f1
Isah Ibn Muhammad <officialibn001@gmail.com>
2024-08-14 18:24:55 +0100

    Fix: Resolved delete user data from database bug 🐛

--- schema/userSchema.js
+++ schema/userSchema.js
@@ -1,246 +1,249 @@
-const { GraphQLObjectType, GraphQLList, GraphQLNonNull, GraphQLString, GraphQLID } = require('graphql')
-const User = require('../config/models/User.js')
-const Project = require('../config/models/Project')
-const Client = require('../config/models/Client')
-const admin = require('firebase-admin')
-const { AuthenticationError } = require('../config/errors/authenticationError.js')
+const {
+  GraphQLObjectType,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLString,
+  GraphQLID,
+} = require("graphql");
+const User = require("../config/models/User.js");
+const Project = require("../config/models/Project");
+const Client = require("../config/models/Client");
+const admin = require("firebase-admin");
+const {
+  AuthenticationError,
+} = require("../config/errors/authenticationError.js");
 
 const UserType = new GraphQLObjectType({
-    name: 'User',
-    fields: () => {
-        const { ProjectType } = require('./projectsSchema.js')
-        const { ClientType } = require('./clientSchema.js')
+  name: "User",
+  fields: () => {
+    const { ProjectType } = require("./projectsSchema.js");
+    const { ClientType } = require("./clientSchema.js");
 
-        return {
-            id: { type: GraphQLID },
-            uid: { type: GraphQLString },
-            email: { type: GraphQLString },
-            name: { type: GraphQLString },
-            profileUrl: { type: GraphQLString },
-            projects: {
-                type: new GraphQLList(ProjectType),
-                async resolve(parent, args) {
-                    return await Project.find({ _id: { $in: parent.projects } })
-                }
-            },
-            clients: {
-                type: new GraphQLList(ClientType),
-                async resolve(parent, args) {
-                    return await Client.find({ _id: { $in: parent.clients } })
-                }
-            }
-        }
-    }
-})
+    return {
+      id: { type: GraphQLID },
+      uid: { type: GraphQLString },
+      email: { type: GraphQLString },
+      name: { type: GraphQLString },
+      profileUrl: { type: GraphQLString },
+      projects: {
+        type: new GraphQLList(ProjectType),
+        async resolve(parent, args) {
+          return await Project.find({ _id: { $in: parent.projects } });
+        },
+      },
+      clients: {
+        type: new GraphQLList(ClientType),
+        async resolve(parent, args) {
+          return await Client.find({ _id: { $in: parent.clients } });
+        },
+      },
+    };
+  },
+});
 
 const UserQuery = {
-    user: {
-        type: UserType,
-        args: {
-            id: {
-                type: new GraphQLNonNull(GraphQLID),
-            }
-        },
-        async resolve(parent, args) {
-            return await User.findByID(args.id)
-        }
-    }
-}
+  user: {
+    type: UserType,
+    args: {
+      id: {
+        type: new GraphQLNonNull(GraphQLID),
+      },
+    },
+    async resolve(parent, args) {
+      return await User.findByID(args.id);
+    },
+  },
+};
 
 const UserMutation = {
-    signUp: {
-        type: UserType,
-        args: {
-            uid: {
-                type: new GraphQLNonNull(GraphQLID)
-            },
-            email: {
-                type: new GraphQLNonNull(GraphQLString)
-            },
-            displayName: {
-                type: new GraphQLNonNull(GraphQLString)
-            },
-
-        },
-        async resolve(parent, args) {
-            try {
-
-                const newUser = new User({
-                    uid: args.uid,
-                    email: args.email,
-                    name: args.displayName,
-                    projects: [],
-                    clients: [],
-                })
-
-                return newUser.save()
-            } catch (error) {
-                return new Error(error.message)
-            }
-        }
+  signUp: {
+    type: UserType,
+    args: {
+      uid: {
+        type: new GraphQLNonNull(GraphQLID),
+      },
+      email: {
+        type: new GraphQLNonNull(GraphQLString),
+      },
+      displayName: {
+        type: new GraphQLNonNull(GraphQLString),
+      },
     },
-    signIn: {
-        type: UserType,
-        args: {
-            uid: { type: new GraphQLNonNull(GraphQLID) },
-            email: { type: GraphQLString }
-        },
-        async resolve(parent, args) {
-            try {
-                const user = await admin.auth().getUser(args.uid)
+    async resolve(parent, args) {
+      try {
+        const newUser = new User({
+          uid: args.uid,
+          email: args.email,
+          name: args.displayName,
+          projects: [],
+          clients: [],
+        });
 
-                if (!user) {
-                    throw new AuthenticationError()
-                }
-
-                return await User.findOne({ uid: { $in: args.uid } })
-            } catch (error) {
-                console.log(`Firebase Error: ${error}`.blue.underline);
-
-                return new Error(error.message)
-
-            }
-        }
+        return newUser.save();
+      } catch (error) {
+        return new Error(error.message);
+      }
     },
-    updateProfile: {
-        type: UserType,
-        args: {
-            uid: { type: new GraphQLNonNull(GraphQLID) },
-            name: { type: new GraphQLNonNull(GraphQLString) },
-            email: { type: new GraphQLNonNull(GraphQLString) },
-            profileUrl: { type: GraphQLString },
-        },
-        async resolve(parent, { uid, name, email, profileUrl }, context) {
-            try {
-                const user = await admin.auth().getUser(uid)
-
-                if (!user) {
-                    throw new AuthenticationError()
-                }
-
-                const existingUser = await User.findOne({ uid })
-
-
-                await admin.auth().updateUser(uid, {
-                    email,
-                    displayName: name,
-                    photoURL: profileUrl ? profileUrl : user.profileUrl,
-                    emailVerified: user.email === email
-                })
-
-
-                const newUserData = await User.findByIdAndUpdate(existingUser._id, {
-                    name: name || existingUser.name,
-                    email: email || existingUser.email,
-                    profileUrl: profileUrl || existingUser.profileUrl
-                })
-
-                return newUserData
-            } catch (error) {
-                console.log(error);
-                console.log(JSON.stringify(error, null, 2));
-
-                throw new Error(error)
-            }
-        }
+  },
+  signIn: {
+    type: UserType,
+    args: {
+      uid: { type: new GraphQLNonNull(GraphQLID) },
+      email: { type: GraphQLString },
     },
-    updatePassword: {
-        type: UserType,
-        args: {
-            uid: { type: new GraphQLNonNull(GraphQLID) },
-            password: { type: new GraphQLNonNull(GraphQLString) },
+    async resolve(parent, args) {
+      try {
+        const user = await admin.auth().getUser(args.uid);
 
-        },
-        async resolve(parent, { uid, password }, context) {
-            try {
-                const user = await admin.auth().getUser(uid)
-
-                if (!user) {
-                    throw new AuthenticationError()
-                }
-
-                await admin.auth().updateUser(uid, {
-                    password
-                })
-
-                const existingUser = await User.findOne({ uid })
-
-                return existingUser
-            } catch (error) {
-                console.log(error);
-                console.log(JSON.stringify(error, null, 2));
-
-                throw new Error(error)
-            }
+        if (!user) {
+          throw new AuthenticationError();
         }
+
+        return await User.findOne({ uid: { $in: args.uid } });
+      } catch (error) {
+        console.log(`Firebase Error: ${error}`.blue.underline);
+
+        return new Error(error.message);
+      }
     },
-    eraseData: {
-        type: UserType,
-        args: {
-            uid: { type: new GraphQLNonNull(GraphQLID) }
-        },
-        async resolve(parent, { uid }, context) {
-
-            if (!context.user) {
-                throw new AuthenticationError()
-            }
-
-            const contextUID = await context.user.uid
-
-            if (contextUID !== uid) {
-                throw new Error('You don\'t have the permission to perform this operation')
-            }
-
-            try {
-                await Project.deleteMany({ contextUID })
-
-                await Client.deleteMany({ contextUID })
-
-                return {
-                    message: 'Data erased successfully!'
-                }
-            } catch (error) {
-                console.log('Erasing User Data Error: ', error);
-
-                throw new Error(error)
-
-
-            }
-        }
+  },
+  updateProfile: {
+    type: UserType,
+    args: {
+      uid: { type: new GraphQLNonNull(GraphQLID) },
+      name: { type: new GraphQLNonNull(GraphQLString) },
+      email: { type: new GraphQLNonNull(GraphQLString) },
+      profileUrl: { type: GraphQLString },
     },
-    deleteAccount: {
-        type: UserType,
-        args: {
-            uid: { type: new GraphQLNonNull(GraphQLID) }
-        },
-        async resolve(parent, { uid }, context) {
-            if (!context.user) {
-                throw new AuthenticationError()
-            }
+    async resolve(parent, { uid, name, email, profileUrl }, context) {
+      try {
+        const user = await admin.auth().getUser(uid);
 
-            const contextUID = await context?.user.uid
-
-            if (contextUID !== uid) {
-                throw new Error('You don\'t have permission to perform this operation')
-            }
-
-            try {
-                await Project.deleteMany({ contextUID })
-
-                await Client.deleteMany({ contextUID })
-
-                await admin.auth().deleteUser(uid)
-
-                return {
-                    message: 'Data erased successfully!'
-                }
-            } catch (error) {
-                console.log('Erasing User Data Error: ', error);
-
-                throw new Error(error)
-            }
+        if (!user) {
+          throw new AuthenticationError();
         }
-    }
-}
 
-module.exports = { UserType, UserMutation, UserQuery }
+        const existingUser = await User.findOne({ uid });
+
+        await admin.auth().updateUser(uid, {
+          email,
+          displayName: name,
+          photoURL: profileUrl ? profileUrl : user.profileUrl,
+          emailVerified: user.email === email,
+        });
+
+        const newUserData = await User.findByIdAndUpdate(existingUser._id, {
+          name: name || existingUser.name,
+          email: email || existingUser.email,
+          profileUrl: profileUrl || existingUser.profileUrl,
+        });
+
+        return newUserData;
+      } catch (error) {
+        console.log(error);
+        console.log(JSON.stringify(error, null, 2));
+
+        throw new Error(error);
+      }
+    },
+  },
+  updatePassword: {
+    type: UserType,
+    args: {
+      uid: { type: new GraphQLNonNull(GraphQLID) },
+      password: { type: new GraphQLNonNull(GraphQLString) },
+    },
+    async resolve(parent, { uid, password }, context) {
+      try {
+        const user = await admin.auth().getUser(uid);
+
+        if (!user) {
+          throw new AuthenticationError();
+        }
+
+        await admin.auth().updateUser(uid, {
+          password,
+        });
+
+        const existingUser = await User.findOne({ uid });
+
+        return existingUser;
+      } catch (error) {
+        console.log(error);
+        console.log(JSON.stringify(error, null, 2));
+
+        throw new Error(error);
+      }
+    },
+  },
+  eraseData: {
+    type: UserType,
+    args: {
+      uid: { type: new GraphQLNonNull(GraphQLID) },
+    },
+    async resolve(parent, { uid }, context) {
+      if (!context.user) {
+        throw new AuthenticationError();
+      }
+
+      const userUID = await context.user.uid;
+
+      if (userUID !== uid) {
+        throw new Error(
+          "You don't have the permission to perform this operation",
+        );
+      }
+
+      try {
+        const res1 = await Project.deleteMany({ userUID });
+
+        const res2 = await Client.deleteMany({ userUID });
+
+        return {
+          message: "Data erased successfully!",
+        };
+      } catch (error) {
+        console.log("Erasing User Data Error: ", error);
+
+        throw new Error(error);
+      }
+    },
+  },
+  deleteAccount: {
+    type: UserType,
+    args: {
+      uid: { type: new GraphQLNonNull(GraphQLID) },
+    },
+    async resolve(parent, { uid }, context) {
+      if (!context.user) {
+        throw new AuthenticationError();
+      }
+
+      const userUID = await context?.user.uid;
+
+      if (userUID !== uid) {
+        throw new Error("You don't have permission to perform this operation");
+      }
+
+      try {
+        await Project.deleteMany({ userUID });
+
+        await Client.deleteMany({ userUID });
+
+        await User.deleteOne({ userUID });
+
+        await admin.auth().deleteUser(uid);
+
+        return {
+          message: "Data erased successfully!",
+        };
+      } catch (error) {
+        console.log("Erasing User Data Error: ", error);
+
+        throw new Error(error);
+      }
+    },
+  },
+};
+
+module.exports = { UserType, UserMutation, UserQuery };
